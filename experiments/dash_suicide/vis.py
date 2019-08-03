e3c63c7c210c524bd023dba9227b9a983a6f24a9
gertsfert <gertsfert@gmail.com>
2019-08-03 15:59:16 +1000

    non functional graph

--- experiments/dash_suicide/vis.py
+++ experiments/dash_suicide/vis.py
@@ -1,10 +1,16 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly_express as px
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv(r"data/suicides.csv")
+
+fig = px.line(df, x="year", y="country")
 
 app.layout = html.Div(
     children=[
@@ -14,7 +20,9 @@ app.layout = html.Div(
         Dash: A web application framework for Python.
     """
         ),
-        id,
+        dcc.Graph(id="suicides", figure=fig),
     ]
 )
 
+if __name__ == "__main__":
+    app.run_server(debug=True)
